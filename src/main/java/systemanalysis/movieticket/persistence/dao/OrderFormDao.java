2759265940568ce9b0a4dc6841f2564f46aa3586
li zifeng <476108172@qq.com>
2017-04-22 00:53:58 +0800

    define the interface of OderformDao

--- src/main/java/systemanalysis/movieticket/persistence/dao/OrderFormDao.java
+++ src/main/java/systemanalysis/movieticket/persistence/dao/OrderFormDao.java
@@ -1,7 +1,25 @@
 package systemanalysis.movieticket.persistence.dao;
 
+import java.util.List;
+
+import systemanalysis.movieticket.persistence.entity.OrderForm;
+import systemanalysis.movieticket.persistence.entity.Preference;
+
 public interface OrderFormDao {
+	
 	//Search order by emailaddress
 	//Delete orderform by oid
 	//Delete all relevant orderform by emailaddress
+	OrderForm findOne(String emailaddress);
+
+    void create(OrderForm entity);
+
+    OrderForm update(OrderForm entity);
+
+    void delete(OrderForm entity);
+
+    void deleteById(OrderForm entityemailaddress);
+    
+    //Search all relevant preference by emailaddress and return in type List<Preference>
+    //List<OrderForm> searchAllRelevant(String emailaddress);
 }
