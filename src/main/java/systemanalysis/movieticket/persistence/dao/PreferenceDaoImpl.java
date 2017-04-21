84fa9d044c9cbeef8275498431ac30a85453ccbc
li zifeng <476108172@qq.com>
2017-04-22 00:44:18 +0800

    implement of the new function searchAllRelevant

--- src/main/java/systemanalysis/movieticket/persistence/dao/PreferenceDaoImpl.java
+++ src/main/java/systemanalysis/movieticket/persistence/dao/PreferenceDaoImpl.java
@@ -1,5 +1,9 @@
 package systemanalysis.movieticket.persistence.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import systemanalysis.movieticket.persistence.entity.Preference;
 
 public class PreferenceDaoImpl extends AbstractJpaDAO<Preference> implements PreferenceDao {
@@ -17,4 +21,12 @@ public class PreferenceDaoImpl extends AbstractJpaDAO<Preference> implements Pre
 		final Preference entity = findOne(entityemailaddress);
 		delete(entity);
 	}
+	
+	public List<Preference> searchAllRelevant(String emailaddress) {
+		Query query = entityManager.createQuery("selete * from Preference where emailaddress = " + emailaddress);
+		List<Preference> result = query.getResultList();
+		return result; 
+	}
+	
+	
 }
